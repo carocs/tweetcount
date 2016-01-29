9ddae24a1310d623a28a0c5987621bfa8da6743f
Igor Lemes <igorlemes.ti@gmail.com>
2016-01-29 06:19:28 -0200

    commit requirements

--- tweetcount/settings.py
+++ tweetcount/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,17 +76,18 @@ WSGI_APPLICATION = 'tweetcount.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tweetdb',
-        'USER': 'postgres',
-        'PASSWORD': 'dbadmin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'tweetdb',
+#        'USER': 'postgres',
+#        'PASSWORD': 'dbadmin',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
